3e91ab116ac5ea34d8eec4905f39349142a10ed1
kokkiemouse <kokkiemouse@gmail.com>
2020-07-29 07:22:50 +0900

    edited

---
+++ src/main/java/io/github/kokkiemouse/yjsnpi_mod/yjsnpimodMain.java
@@ -0,0 +1,11 @@
+package io.github.kokkiemouse.yjsnpi_mod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class yjsnpimodMain implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        System.out.println("Hello Fabric world!");
+    }
+}
